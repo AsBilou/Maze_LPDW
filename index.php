8b3564de532b53d4adc42763b8c6ee3567da5e62
Romain Bellina <contact@romainbellina.fr>
2012-11-28 10:07:39 +0100

    Add google Analytics

--- index.php
+++ index.php
@@ -31,6 +31,17 @@ $maze->buildMaze($start_cell);
         <link rel="stylesheet" media="screen" href="bootstrap.css">
         <title>Maze 3000</title>
         <?php $maze->convertPhpToJavascript($maze); ?>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-36652122-1']);
+            _gaq.push(['_trackPageview']);
+            
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
     </head>
     <body onkeyup="touche(Event);">
         <script  type="text/javascript" src="deplacement.js"></script>
