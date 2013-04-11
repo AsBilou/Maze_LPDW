82daa94add5e639e436671506c51042e97adc7ec
AsBilou <contact@romainbellina.fr>
2013-04-11 11:50:45 +0300

    Remove google analytics and add piwik

--- index.php
+++ index.php
@@ -47,18 +47,23 @@ $maze->buildMaze($start_cell);
         <link rel="icon" type="image/png" href="img/favicon.png" />
         <!-- Conversion du tableau maze php en tabeau javascript avec filtre des données. -->
         <?php $maze->convertPhpToJavascript($maze); ?>
-        <!-- Google Analytics -->
+
+        <!-- Piwik -->
         <script type="text/javascript">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-36652122-1']);
-            _gaq.push(['_trackPageview']);
-            
+            var _paq = _paq || [];
+            _paq.push(["trackPageView"]);
+            _paq.push(["enableLinkTracking"]);
+
             (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.bilou.net/";
+                _paq.push(["setTrackerUrl", u+"piwik.php"]);
+                _paq.push(["setSiteId", "1"]);
+                var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+                g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
             })();
         </script>
+        <!-- End Piwik Code -->
+        
     </head>
     <body>
     
